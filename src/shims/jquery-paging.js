e56559fcd621fbb84b3d3a0596f45ceb1eb38148
Hal Blackburn <hwtb2@cam.ac.uk>
2015-11-09 14:13:24 +0000

    Add alias for jquery-paging

--- src/shims/jquery-paging.js
+++ src/shims/jquery-paging.js
@@ -1,10 +1,25 @@
+import WebpackConfig from 'webpack-config';
+
 import { loader } from '../util';
 
 
-export default loader(
-    {
-        test: /\/jquery\.paging\.js$/,
-        include: /\/bower_components\/paging\//,
-        loader: 'imports?jQuery=jquery,this=>global'
-    }
-);
+export default function() {
+    return new WebpackConfig().merge({
+        resolve: {
+            alias: {
+                // Alias the main file as "paging" is unclear and typing
+                // import 'paging/jquery.paging' in code is annoying.
+                'jquery-paging': 'paging/jquery.paging',
+            }
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\/jquery\.paging\.js$/,
+                    include: /\/bower_components\/paging\//,
+                    loader: 'imports?jQuery=jquery,this=>global'
+                }
+            ]
+        }
+    });
+}
