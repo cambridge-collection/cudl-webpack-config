2e3dde1de9bf9eb2e1a64b5abb09a816768a669f
Hal Blackburn <hwtb2@cam.ac.uk>
2016-03-18 17:41:56 +0000

    Add loaders() to compliment util.loader()
    It allows adding multiple loaders at once, rather than just one.

--- src/util.js
+++ src/util.js
@@ -1,25 +1,42 @@
 import identity from 'lodash/utility/identity';
-import toArray from 'lodash/lang/toArray';
+import isArray from 'lodash/lang/isArray';
 
 import WebpackConfig from 'webpack-config';
 
+/**
+ * Return a function which creates a WebpackConfig with the specified array of
+ * loaders when called.
+ *
+ * If a customiser function is provided, it's invoked when returned function is
+ * invoked, with the defautLoaders array as the first argument followed by any
+ * args the returned function is invoked with. It should return an array of
+ * loaders which will be used in the resulting WebpackConfig.
+ */
+export function loaders(defaultLoaders, customiser = identity) {
+    return (...args) => {
+        if(!isArray(defaultLoaders))
+            throw new Error(
+                `first argument must be an array, got: ${defaultLoaders}`);
 
-export function loader(defaultLoader, customiser) {
-    return function() {
-
-        let loader = (customiser || identity).apply(
-            undefined, [defaultLoader].concat(toArray(arguments)));
+        let loaders = customiser(defaultLoaders, ...args);
 
         return new WebpackConfig().merge({
             module: {
-                loaders: [
-                    loader
-                ]
+                loaders: loaders
             }
         });
     }
-};
+}
 
+/**
+ * As loaders(), except the function itself and the customiser take a single
+ * loader, not an array of loaders.
+ */
+export function loader(defaultLoader, customiser = identity) {
+    return loaders([defaultLoader], ([defaultLoader], ...args) => {
+        return [customiser(defaultLoader, ...args)];
+    })
+}
 
 /**
  * Lookup a value from WebpackConfig.environment.
