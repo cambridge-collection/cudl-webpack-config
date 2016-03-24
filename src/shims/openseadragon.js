f5f1946b3d6f1a4fc7cd57c974850cf786720ff8
Hal Blackburn <hwtb2@cam.ac.uk>
2016-03-24 16:24:08 +0000

    Support shimming OpenSeadragon 2.1.0

--- src/shims/openseadragon.js
+++ src/shims/openseadragon.js
@@ -1,9 +1,61 @@
 import WebpackConfig from 'webpack-config';
 
-import { loader } from '../util';
+import { loaders } from '../util';
 
+export function shim_2_1_0() {
+    // OpenSeadragon still has non-standard packaging which requires lots of
+    // hacking. They're about to do a release on NPM which should be more
+    // regular...
+    let dirPattern = /\/node_modules\/OpenSeadragon\/src\//;
 
-export default function() {
+    return new WebpackConfig().merge({
+        module: {
+            loaders: [
+                {
+                    include: dirPattern,
+                    test: /\/.*\.js$/,
+                    exclude: /\/openseadragon\.js/,
+                    loader: 'imports?OpenSeadragon=./openseadragon.js'
+                },
+                {
+                    include: dirPattern,
+                    test: /\/openseadragon\.js$/,
+                    loaders: [
+                        'imports?window=>global',
+                        'exports?OpenSeadragon',
+                        'wrap?shim-openseadragon-2.1.0'
+                    ]
+                }
+            ]
+        },
+        wrap: {
+            'shim-openseadragon-2.1.0': {
+                before: `\
+// Redirect assignment of OpenSeadragon global to local var. This is required
+// as otherwise we can't assign the OpenSeadragon local before it's passed to
+// the main module wrapping function.
+var OpenSeadragon;
+Object.defineProperty(window, 'OpenSeadragon', {
+    get: function() { return OpenSeadragon; },
+    set: function(value) { OpenSeadragon = value; },
+    configurable: true,
+    enumerable: true
+});
+`,
+                after: `\
+// Clean up our intercepting property
+'delete window.OpenSeadragon;'
+`
+            }
+        }
+    });
+}
+
+export default function(version) {
+    if(version ==='2.1.0') {
+        return shim_2_1_0();
+    }
+
     return new WebpackConfig().merge({
         resolve: {
             alias: {
@@ -23,3 +75,4 @@ export default function() {
         }
     });
 }
+
